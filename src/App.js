f79798b3a8a8647977ca42a24f697ed8259a769d
Vilzku <ville.puolitaival@gmail.com>
2021-01-13 18:17:03 +0200

    Fetch all areas

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 
 import React, { useEffect, useState } from 'react';
-import { fetchCityData, fetchLocalData } from './thl_api.js';
+import { fetchAreaData, fetchLocalData } from './thl_api.js';
 
 import './App.css';
 import LayoutLeft from './Components/LayoutLeft';
 import LayoutRight from './Components/LayoutRight';
 
 function App() {
+  const [hcdList, setHcdList] = useState([]);
   const [cityList, setCityList] = useState([]);
   const [selectedCity, setSelectedCity] = useState(null);
 
   function selectCity() {
     fetchLocalData().then(data => {
       setSelectedCity(data);
-      console.log("selectCity(): " + data.city + " ladattu");
+      console.log("selectCity: " + data.city + " ladattu");
     })
   }
 
-  useEffect( () => {
-    fetchCityData().then(data => {
-      setCityList(data);
-    })
+  const ALL_AREAS_ID = '445222'
+
+  useEffect(() => {
+    fetchAreaData(ALL_AREAS_ID).then(hcddata => {
+      setHcdList(hcddata);
+
+      let temp_list = []
+      for(let i=0; i<hcddata.length; i++) {
+        if(hcddata[i].key !== ALL_AREAS_ID) {
+          fetchAreaData(hcddata[i].key).then(citydata => {
+            temp_list = [...temp_list, ...citydata]
+            temp_list.sort((a, b) => (a.name > b.name) ? 1 : -1)
+            setCityList(temp_list);
+          });
+        }
+      }
+    });
   }, []);
 
 
--- src/thl_api.js
+++ src/thl_api.js
@@ -1,5 +1,6 @@
 
-export let fetchCityData = () => {
+
+export function fetchAreaData(id) {
 
 /*
   Returns a promise with a list of objects { key: x, name: y, cases: z }
@@ -7,44 +8,76 @@ export let fetchCityData = () => {
 
   return new Promise(resolve => {
 
-    const url = 'https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.json?row=hcdmunicipality2020-445043&column=dateweek20200101-509030'
+    const url = 'https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.json?row=hcdmunicipality2020-' + id + '&column=dateweek20200101-509030'
     let list = []
 
     fetch(url)
       .then(response => {
         response.json()
           .then(data => {
-          let citylist = data.dataset.dimension.hcdmunicipality2020.category.label;
-            for(let k in citylist) {
+          let areas = data.dataset.dimension.hcdmunicipality2020.category.label;
+      
+            // Add each area to the list
+            for(let k in areas) {
               list.push({
                 key: k,
-                name: citylist[k],
-                cases: 0
+                name: areas[k],
               });
             }
+
+            // Sort the list in alphabetical order
+            list.sort((a, b) => (a.name > b.name) ? 1 : -1)
+
+
+            /*
+              TODO:
+              Alueiden ja casejen eri järjestys (alueet id->nouseva ja caset alueiden mukainen aakkosjärjestys) laittaa caset väärään    järjestykseen
+            */
+
+            // Insert covid data to each object
+            let index = 1;
+            for(let i=0; i<list.length; i++) {
+              let value = data.dataset.value[index++*106-1];
+              list[i]['cases'] = value;
+            }
+
+            /*
+              TODO:
+              Se millä haetaan (id parametri) pitää poistaa listasta, jotta sitä ei palauteta uudestaan, sillä se on jo kerran haettu
+            */
+
+            //Remove the area that is used for the search (id)
+            /* ??? */
+
+            //console.log(JSON.stringify(list))
             return resolve(list);
             
+          })
+          .catch(err => {
+            console.log(err);
+            setTimeout(() => { return fetchAreaData(id); }, 1000);
           })
       })
       .catch(err => {
         console.log(err);
-        return [{key: "000000", name: "Jotain meni pieleen", cases: -1}]
+        setTimeout(() => { return fetchAreaData(id); }, 1000);
       })
-
   })
-    
 }
 
 
 
-export let fetchLocalData = () => {
+export function fetchLocalData() {
 
 /*
   Returns a promise with a object
   { key: x, city: y, weeklyCases: [{ 0-105: value }] }
 */
 
-  /* TODO: Valittu kunta hardcodattu tällä hetkellä testaamista varten */
+    /*
+      TODO:
+      Valittu kunta hardcodattu tällä hetkellä testaamista varten
+    */
 
   return new Promise(resolve => {
   
@@ -62,12 +95,14 @@ export let fetchLocalData = () => {
           }
           return resolve(localData);
         })
+        .catch(err => {
+          console.log(err);
+          return [{key: "000000", name: "Jotain meni pieleen (-103)", weeklyCases: -1}]
+        })
     })
-    .catch(err => { 
+    .catch(err => {
       console.log(err);
-      return {key: "000000", city: "Jotain meni pieleen", weeklyCases: -1};
+      return [{key: "000000", name: "Jotain meni pieleen (-104)", weeklyCases: -1}]
     })
-    
   })
-
 }
