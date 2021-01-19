29676cc7c795d9de8405bffd0af52836fb6da651
Siirabs <63456517+Siirabs@users.noreply.github.com>
2021-01-19 19:10:01 +0200

    Oispa veleho

--- src/Components/LayoutRight/CumulativeGraph.jsx
+++ src/Components/LayoutRight/CumulativeGraph.jsx
@@ -4,25 +4,37 @@ import CanvasJSReact from '../../Assets/canvasjs.react';
 
 var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-var dps = [];
+
 
 function CumulativeGraph(props) {
     let weeklyHcdCases = props.selectedCity.weeklyHcdCases;
     /*if(!selectedCity) return (<div className="CumulativeGraph"></div>)*/
     /*let weeklyHcdCases = props.weeklyHcdCases;*/
-    var week = 1;
+    let dps = [];
+    let week = 1;
+    let sum = 0;
     /*console.log(selectedCity.area);*/
-    console.log(dps);
+    
     console.log(weeklyHcdCases);
-    for(let i in weeklyHcdCases) {
-        dps.push({x: weeklyHcdCases, y: week})
-        i++;
-        week++;
+    
+        for(let i in weeklyHcdCases) {
+            if(weeklyHcdCases[i] === undefined) {
+                continue;
+            }
+            /*console.log({x: weeklyHcdCases[i], y: week});*/
+            sum += parseInt(weeklyHcdCases[i])
+            dps.push({x: parseInt(week), y: parseInt(sum)})
         
-    }
-    week++;
-    console.log(week);
-    /*dps.push({x: 1, y:2})*/
+            week++;
+            
+        }
+        dps.pop()
+    
+      
+        console.log(JSON.stringify(dps));
+    
+    
+    
     
     const options = {
         theme: "light2",
@@ -30,16 +42,20 @@ function CumulativeGraph(props) {
         exportEnabled: true,
         title: {
             
-            text:  "Kunnan korona tapaukset"
+            text:  "SHP korona tapaukset"
+        },
+
+        axisX:{
+            reversed: false
         },
         axisY: {
-            title: "Tapaukset viikottain"
+            title: ""
         },
         data: [
         {
             type: "area",
             xValueFormatString: "Viikko #",
-            yValueFormatString: "### Tapausta",
+            yValueFormatString: "#### Tapausta",
             dataPoints: dps
               
         }
